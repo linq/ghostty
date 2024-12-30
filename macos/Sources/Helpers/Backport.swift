7e1d36614249533f5fa376d9bc5e2b73632e8566
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-12-30 23:46:47 +0100

    Drop macos 13 or newer conditionals

--- macos/Sources/Helpers/Backport.swift
+++ macos/Sources/Helpers/Backport.swift
@@ -20,27 +20,11 @@ extension Backport where Content: Scene {
 
 extension Backport where Content: View {
     func pointerVisibility(_ v: BackportVisibility) -> some View {
-        #if canImport(AppKit)
-        if #available(macOS 15, *) {
-            return content.pointerVisibility(v.official)
-        } else {
-            return content
-        }
-        #else
         return content
-        #endif
     }
 
     func pointerStyle(_ style: BackportPointerStyle?) -> some View {
-        #if canImport(AppKit)
-        if #available(macOS 15, *) {
-            return content.pointerStyle(style?.official)
-        } else {
-            return content
-        }
-        #else
         return content
-        #endif
     }
 }
 
@@ -72,24 +56,4 @@ enum BackportPointerStyle {
     case resizeDown
     case resizeUpDown
     case resizeLeftRight
-
-    #if canImport(AppKit)
-    @available(macOS 15, *)
-    var official: PointerStyle {
-        switch self {
-        case .default: return .default
-        case .grabIdle: return .grabIdle
-        case .grabActive: return .grabActive
-        case .horizontalText: return .horizontalText
-        case .verticalText: return .verticalText
-        case .link: return .link
-        case .resizeLeft: return .frameResize(position: .trailing, directions: [.inward])
-        case .resizeRight: return .frameResize(position: .leading, directions: [.inward])
-        case .resizeUp: return .frameResize(position: .bottom, directions: [.inward])
-        case .resizeDown: return .frameResize(position: .top, directions: [.inward])
-        case .resizeUpDown: return .frameResize(position: .top)
-        case .resizeLeftRight: return .frameResize(position: .trailing)
-        }
-    }
-    #endif
 }
